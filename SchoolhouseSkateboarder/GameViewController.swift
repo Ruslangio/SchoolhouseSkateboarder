484449e8679f9832716dff044ab3ac2d08b1dab3
Ruslan <Alekyan_2016@mail.ru>
2022-07-24 20:33:03 +0300

    Initial commit

---
+++ SchoolhouseSkateboarder/GameViewController.swift
@@ -0,0 +1,53 @@
+//
+//  GameViewController.swift
+//  SchoolhouseSkateboarder
+//
+//  Created by Ruslan Alekyan on 13.03.2022.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                let width = view.bounds.width
+                let height = view.bounds.height
+                scene.size = CGSize(width: height, height: width)
+                print(width, height)
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .landscape
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
---
+++ SchoolhouseSkateboarder/Skater.swift
@@ -0,0 +1,42 @@
+//
+//  Skater.swift
+//  SchoolhouseSkateboarder
+//
+//  Created by Ruslan Alekyan on 14.03.2022.
+//
+
+import SpriteKit
+
+class Skater: SKSpriteNode {
+    var velocity = CGPoint.zero
+    var minimumY: CGFloat = 0.0
+    var jumpSpeed: CGFloat = 20.0
+    var isOnGround = true
+    
+    func setupPhysicsBody() {
+        if let skaterTexture = texture {
+            physicsBody = SKPhysicsBody(texture: skaterTexture, size: size)
+            physicsBody?.isDynamic = true
+            physicsBody?.density = 6.0
+            physicsBody?.allowsRotation = true
+            physicsBody?.angularDamping = 2.0
+            physicsBody?.categoryBitMask = PhysicsCategory.skater
+            physicsBody?.collisionBitMask = PhysicsCategory.brick
+            physicsBody?.contactTestBitMask = PhysicsCategory.brick | PhysicsCategory.gem
+        }
+    }
+    
+    func createSparks() {
+        let bundle = Bundle.main
+        if let sparksPath = bundle.path(forResource: "sparks", ofType: "sks") {
+            let sparksNode = NSKeyedUnarchiver.unarchiveObject(withFile: sparksPath) as! SKEmitterNode
+            sparksNode.position = CGPoint(x: 0.0, y: -50.0)
+            addChild(sparksNode)
+            
+            let waitAction = SKAction.wait(forDuration: 0.5)
+            let removeAction = SKAction.removeFromParent()
+            let waitThenRemove = SKAction.sequence([waitAction, removeAction])
+            sparksNode.run(waitThenRemove)
+        }
+    }
+}
